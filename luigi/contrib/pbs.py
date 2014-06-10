0583a6d6ea7ffeb0f06d86948e458167da0eb668
Dale Roberts <dale.o.roberts@gmail.com>
2014-06-10 17:06:56 +1000

    running task externally

--- luigi/contrib/pbs.py
+++ luigi/contrib/pbs.py
@@ -1,22 +1,63 @@
 """
-PBS
+PBS Job
+-------
+
 """
-import cPickle as pickle
+import pickle
+import signal
+import logging
+import subprocess
+import sys
 import os
 
+import luigi.task
 
-def _dump(self, dir=''):
-    """
-    Dump instance to file.
-    """
-    file_name = os.path.join(dir, 'job-instance.pickle')
-    if self.__module__ == '__main__':
-        d = pickle.dumps(self)
-        module_name = os.path.basename(sys.argv[0]).rsplit('.', 1)[0]
-        d = d.replace('(c__main__', "(c" + module_name)
-        open(file_name, "w").write(d)
-    else:
-        pickle.dump(self, open(file_name, "w"))
+from os.path import abspath, join as pjoin, exists as pexists
+
+TEMPLATE = """
+python -c 'import luigi.contrib.pbs as pbs; pbs.main()
+"""
+
+TASKDIR = '/tmp/tasks'
+TASKFILE = '%s.pickle'
+
+logger = logging.getLogger('luigi-interface')
+
+
+class PbsRunContext(object):
+
+    def __init__(self):
+        self.job_id = None
+
+    def __enter__(self):
+        self.__old_signal = signal.getsignal(signal.SIGTERM)
+        signal.signal(signal.SIGTERM, self.kill_job)
+        return self
+
+    def kill_job(self, captured_signal=None, stack_frame=None):
+        if self.job_id:
+            logger.info('Job interrupted, killing job %s', self.job_id)
+            subprocess.call(['mapred', 'job', '-kill', self.job_id])
+        if captured_signal is not None:
+            # adding 128 gives the exit code corresponding to a signal
+            sys.exit(128 + captured_signal)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is KeyboardInterrupt:
+            self.kill_job()
+        signal.signal(signal.SIGTERM, self.__old_signal)
+
+
+class Runner(object):
+
+    def __init__(self, task=None, filename=None):
+        self.task = task or pickle.load(open(filename))
+
+    def run(self, stdin=sys.stdin, stdout=sys.stdout):
+        dst = self.task.output().path
+        if not os.path.exists(os.path.dirname(dst)):
+            os.makedirs(os.path.dirname(dst))
+        self.task.run()
 
 
 def task_graph(tasks):
@@ -32,7 +73,7 @@ def task_graph(tasks):
         for dep in current.deps():
             stack.append(dep)
             edges.add((current.task_id, dep.task_id))
-            return (list(nodes), list(edges))
+    return (list(nodes), list(edges))
 
 
 def targets(tasks):
@@ -43,8 +84,52 @@ def targets(tasks):
     return nodes
 
 
-def run(tasks, ncpus=1):
+def run(tasks, ncpus=1, filename=TASKFILE):
     """
     Run a list of tasks.
     """
-    pass
+    module_name = os.path.basename(sys.argv[0]).rsplit('.', 1)[0]
+    print 'module_name', module_name
+
+    for task in tasks:
+        filename = pjoin(TASKDIR, TASKFILE % task.task_id)
+        if not os.path.exists(TASKDIR):
+            os.makedirs(TASKDIR)
+        with open(filename, 'w') as f:
+            if task.__module__ == '__main__':
+                d = pickle.dumps(task)
+                d = d.replace('(c__main__', "(c" + module_name)
+                f.write(d)
+            else:
+                pickle.dump(task, f)
+
+    f = __file__
+    if f.endswith('pyc'):
+        f = f[:-3] + 'py'
+    argv = ['python', abspath(f)]
+    popen = subprocess.Popen(argv,
+                             cwd=os.getcwd(),
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+
+    # os.unlink(filename)
+
+
+def print_exception(exc):
+    import traceback
+    tb = traceback.format_exc(exc)
+    print >> sys.stderr, 'luigi-exc-hex=%s' % tb.encode('hex')
+
+
+def main(stdin=sys.stdin, stdout=sys.stdout, print_exception=print_exception):
+    try:
+        logging.basicConfig(level=logging.WARN)
+        taskfiles = [pjoin(TASKDIR,f) for f in os.listdir(TASKDIR) if os.path.isfile(pjoin(TASKDIR, f))]
+        for taskfile in taskfiles:
+            Runner(filename=taskfile).run(stdin=stdin, stdout=stdout)
+    except Exception, exc:
+        print_exception(exc)
+        raise
+
+if __name__ == '__main__':
+    main()
--- test/_pbs.py
+++ test/_pbs.py
@@ -1,6 +1,20 @@
 import unittest
 import luigi
 import luigi.contrib.pbs as pbs
+from os.path import join as pjoin
+
+TEMPDIR = pjoin('/tmp', 'work')
+
+
+class DummyTask(luigi.Task):
+    i = luigi.Parameter()
+
+    def run(self):
+        f = self.output().open('w')
+        f.close()
+
+    def output(self):
+        return luigi.LocalTarget(pjoin(TEMPDIR, str(self.i)))
 
 
 class A(luigi.Task):
@@ -33,10 +47,7 @@ class C(luigi.Task):
         return self.has_run
 
 
-class TestTaskSerialise(unittest.TestCase):
-
-    def setUp(self):
-        pass
+class TestPbs(unittest.TestCase):
 
     def test_targets(self):
         self.assertEqual(sorted(pbs.targets([A()])), ['A()'])
@@ -46,6 +57,19 @@ class TestTaskSerialise(unittest.TestCase):
                          ['A()', 'B()', 'C()'])
         self.assertEqual(sorted(pbs.targets([A(), B(), C()])),
                          ['A()', 'B()', 'C()'])
+
+    def test_run(self):
+        tasks = [DummyTask(i) for i in range(5)]
+        # make sure outputs dont exist
+        for task in tasks:
+            if task.complete():
+                task.output().remove()
+        # run the tasks
+        pbs.run(tasks, ncpus=1)
+        # check for completion
+        for t in tasks:
+            print t.task_id, t.output().path, t.output().exists()
+            #self.assertTrue(t.complete() is True)
 
 
 if __name__ == '__main__':
